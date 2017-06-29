415d331bd819babeee3a4f4a83c20b6acdfac296
Michele Manzato <michele.manzato@newtargetweb.it>
2017-06-29 23:32:50 +0200

    - version: 1.1.3

--- lib/yabt/Main.class.php
+++ lib/yabt/Main.class.php
@@ -15,7 +15,7 @@ namespace yabt;
 //------------------------------------------------------------------------------
 abstract class Main
 {
-	const VERSION="1.1.2";
+	const VERSION="1.1.3";
 
 	public static $confDir = NULL;
 	public static $varDir = '/var';
